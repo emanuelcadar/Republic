6aa882996fcf855386b01e0300588311695de357
Emanuel Cadar <ecadar@gmail.com>
2012-07-09 14:34:14 +0300

    Guilds Landing Page

---
+++ guilds-landing.php
@@ -0,0 +1,175 @@
+<!doctype html>  
+
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> 
+
+<?php  $currentPage = "projectDetails"; ?>
+<?php  $category = "film"; ?>
+<?php  $thisPage = "profile"; ?>
+
+<html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="robots" content="noindex">
+    <title>Republic of Film</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-ipad.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-iphone4.png">
+
+    <link rel="stylesheet" href="css/style.css?v=2">
+    <link rel="stylesheet" href="css/superfish.css">
+    <!-- <link rel="stylesheet" href="css/sparkbox-select.css"> -->
+
+    <script src="js/libs/modernizr-1.7.min.js"></script>
+    
+    <!-- Grab Google CDN's jQuery. fall back to local if necessary -->    
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+    <script src="js/jquery.vote.js"></script>
+    <script src="js/jquery.listmanager.js"></script>
+    <script src="js/jquery.hoverIntent.minified.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/script.js"></script>
+
+
+</head>
+
+<body>
+    <div class="container">
+        
+        <!-- Inlcude Header here... -->
+            
+        <?php include( 'header-simple.php' ); ?>
+            <div class="user-hero guilds-header">
+            	<div class="user-left">
+                	<span class="back-guilds"><a href="#" class="back-home">REPUBLIC HOME</a> / <a href="#">TOP GUILDS</a></span>
+                    <div class="clr"></div>
+                	<a href="#" class="avatar"><img src="images/guild-image.png" width="72" height="72" alt="image"></a>
+                    <div class="user-info group">
+                    	<h2>BROKEN ARROW GUILD</h2>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+                <div class="user-right">
+                	<span class="back-guilds">HEY THERE, MOVIEMAKER.<br> WELCOME TO YOUR GUILD.</span>
+                    <div class="buttons"><a href="#" class="rounded-button">GO TO GUILD BRAINSTORM</a></div>
+                </div>
+            </div>  
+        </header>
+        
+        <div class="main">
+          <div class="guilds">
+          	<div class="guilds-content">
+            	<div class="total-points">
+                	<h2>40,537</h2>
+                    <div class="ranks">
+                    	<p><span>GUILD<br> RANK</span><strong>13th</strong></p>
+                        <div class="tp-a">
+                        	<span>TOTAL POINTS <br>ACCUMULATED</span>
+                        </div>
+                    </div>
+                    <div class="guilds-status">
+                        <p><strong>75</strong><span>Winning <br> Pitches</span> / <strong>265</strong><span>Total <br> Pitches</span>
+                        <p class="smaller-p"><strong>13.2%</strong><span>WINNING PITCH<br>PERCENTAGE</span>  <strong>75</strong><span>POINTS PER <br> PITCH AVG</span>
+                    </div>
+                </div>
+                <div class="other-infos group">
+                	<div class="guilds-status">
+                        <p><strong>537</strong><span>POINTS <br> THIS YEAR</span>  <strong>150</strong><span>POINTS <br> THIS MONTH</span> <strong>67</strong><span>POINTS <br> THIS WEEK</span></p>
+                        <p class="smaller-p rep-e"><strong>17.2</strong><span>AVG POINTS<br>PER PITCH</span>  <strong>17.2</strong><span>AVG POINTS<br>PER PITCH</span>  <strong>30.2</strong><span>AVG POINTS<br>PER PITCH</span> </p>
+                        <p class="smaller-p"><span>Guild<br>rank</span><strong>5th</strong>  <span>Guild<br>rank</span><strong>5th</strong> <span>Guild<br>rank</span><strong>5th</strong></p>
+                    </div>
+                </div>
+                <div class="other-infos group">
+                	<div class="guilds-status">
+                    	<div class="graph-guilds">
+                        	<img src="images/graph.png" width="173" height="163" alt="graph">
+                            <div class="points-res">
+                                <p><strong>3,181</strong><span>SCENE PITCH  <br> POINTS</span> </p>
+                                <p><strong>1,356</strong><span>COMMENTS <br> POINTS</span> </p>
+                            </div>
+                        </div>
+                        <div class="points-timeline">
+                        	<h5>POINTS TIMELINE</h5>
+                            <img src="images/img.png" width="255" height="115" alt="timeline">
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>    
+        </div>
+        <div class="guilds-sidebar">
+        			<h3>MEMBERS</h3>
+								<div class="pit-user odd-pit group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user odd-pit group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user odd-pit group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user odd-pit group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user odd-pit group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user odd-pit group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user odd-pit group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                                <div class="pit-user group">
+                                	<a href="#"><img src="images/user.png" width="34" height="34" alt="user"></a>
+                                    <span class="pt-name">moviemaker</span>
+                                </div>
+                   
+               </div>
+          </div>
+    </div> <!--! end of #container -->
+    
+    <!-- Inlcude Footer here... -->        
+    <?php include( 'footer.php' ); ?>
