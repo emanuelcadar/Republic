25018ed152d6c37a40a0dc1579248eca48a9da31
Emanuel Cadar <ecadar@gmail.com>
2012-07-12 13:30:43 +0300

    Guilds Post Thread Page

---
+++ guild-post-thread.php
@@ -0,0 +1,213 @@
+<!doctype html>  
+
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> 
+
+<?php  $currentPage = "film"; ?>
+<?php  $category = "film"; ?>
+<?php  $thisPage = "film"; ?>
+
+<html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="robots" content="noindex">
+    <title>Republic of Film</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-ipad.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-iphone4.png">
+
+    <link rel="stylesheet" href="css/style.css?v=2">
+    <link rel="stylesheet" href="css/superfish.css">
+    <!-- <link rel="stylesheet" href="css/sparkbox-select.css"> -->
+
+    <script src="js/libs/modernizr-1.7.min.js"></script>
+    
+    <!-- Grab Google CDN's jQuery. fall back to local if necessary -->    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
+    <script src="js/jquery.ui.selectmenu.js"></script>
+    <script src="js/jquery.vote.js"></script>
+    <script src="js/jquery.listmanager.js"></script>
+    <script src="js/jquery.hoverIntent.minified.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/script.js"></script>
+    <script type="text/javascript">
+		$(function(){
+			$('select.sel').selectmenu();
+		});
+	</script>
+
+</head>
+
+<body>
+    <div class="container">
+        
+        <!-- Inlcude Header here... -->
+            
+        <?php include( 'header-simple.php' ); ?>
+            <div class="user-hero guilds-header">
+            	<div class="user-left">
+                	<span class="back-guilds"><a href="#" class="back-home">REPUBLIC HOME</a> / <a href="#">TOP GUILDS</a> / <a href="#">GUILD HOME</a></span>
+                    <div class="clr"></div>
+                	<a href="#" class="avatar"><img src="images/guild-image.png" width="72" height="72" alt="image"></a>
+                    <div class="user-info group">
+                    	<h2>BROKEN ARROW GUILD</h2>
+                        <span><a href="#">GUILD FORUM</a></span>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+                <div class="user-right emp">
+                    <div class="buttons"><a href="#" class="rounded-button">POST NEW THREAD</a></div>
+                </div>
+            </div>  
+        </header>
+        
+        <div class="main">
+            <section>
+            <div class="brainstorm">
+            <ul>
+              <li class="single standard-listing">
+				<div class="avatar-l">
+                	<a href="#"><img src="images/user.png" width="74" height="74" alt="user"></a>
+                </div>
+                <h3>Upon arrival home from the spa, Janet walks into her 3 bedroom apartment and discovers Barkley has completed destroyed her wedding dress. Not only that but he has ate the wedding ring, and wrecked Janet's apartment. Janet sees all of this, but is in for the biggest surprise of all, a hidden box!</h3>
+                <p>When Janet comes home to Barkley and her apartment, she notices something is off. First of all her couch cushions are missing. Not destroyed, but completely missing - as in nowhere to be found.  She immediately panics and go straight for the Herman Miller chair in which her wedding dress laid. After seeing the total annihilation  of the dress of her dreams she dropped to her knees, only to find the empty box that contained her wedding ring! she had taken it off while at the spa and now it was missing. She looked over to Barkley. Barkley could not even make eye contact. He knew he was a bad boy. Janet burst into tears. She wiped her face and looked down at a box she had never seen before. it was bright pink and wrapped in a erotic lace ribbon. Inside Janet sees countless notes labeled Chris and Chrissy-Poo covered in lipstick stains. The box smelled of cheap cologne and vodka. "Is Chris cheating on me!!!? she exclaimed.."</p>
+                <ul class="meta">
+                    <li>By: <a class="user" href="user-profile.php">MovieMaker</a></li>
+                    <li><a class="to-comments" href="scene-comments.php">26 Comments</a></li>
+                    <li><a class="read-more" id="show-hide" href="#">Read entire scene</a></li>
+                </ul>
+                <div class="img-r">
+                	<img src="images/recent_activity2_07.png" width="208" height="118" alt="scene">
+                </div>
+              </li>
+              <li class="single standard-listing">
+				<div class="avatar-l">
+                	<a href="#"><img src="images/user.png" width="74" height="74" alt="user"></a>
+                </div>
+                <h3>Upon arrival home from the spa, Janet walks into her 3 bedroom apartment and discovers Barkley has completed destroyed her wedding dress. Not only that but he has ate the wedding ring, and wrecked Janet's apartment. Janet sees all of this, but is in for the biggest surprise of all, a hidden box!</h3>
+                <p>When Janet comes home to Barkley and her apartment, she notices something is off. First of all her couch cushions are missing. Not destroyed, but completely missing - as in nowhere to be found.  She immediately panics and go straight for the Herman Miller chair in which her wedding dress laid. After seeing the total annihilation  of the dress of her dreams she dropped to her knees, only to find the empty box that contained her wedding ring! she had taken it off while at the spa and now it was missing. She looked over to Barkley. Barkley could not even make eye contact. He knew he was a bad boy. Janet burst into tears. She wiped her face and looked down at a box she had never seen before. it was bright pink and wrapped in a erotic lace ribbon. Inside Janet sees countless notes labeled Chris and Chrissy-Poo covered in lipstick stains. The box smelled of cheap cologne and vodka. "Is Chris cheating on me!!!? she exclaimed.."</p>
+                <ul class="meta">
+                    <li>By: <a class="user" href="user-profile.php">MovieMaker</a></li>
+                    <li><a class="to-comments" href="scene-comments.php">26 Comments</a></li>
+                    <li><a class="read-more" id="show-hide" href="#">Read entire scene</a></li>
+                </ul>
+                <div class="img-r">
+                	<img src="images/recent_activity2_07.png" width="208" height="118" alt="scene">
+                </div>
+              </li>
+              <li class="single standard-listing">
+				<div class="avatar-l">
+                	<a href="#"><img src="images/user.png" width="74" height="74" alt="user"></a>
+                </div>
+                <h3>Upon arrival home from the spa, Janet walks into her 3 bedroom apartment and discovers Barkley has completed destroyed her wedding dress. Not only that but he has ate the wedding ring, and wrecked Janet's apartment. Janet sees all of this, but is in for the biggest surprise of all, a hidden box!</h3>
+                <p>When Janet comes home to Barkley and her apartment, she notices something is off. First of all her couch cushions are missing. Not destroyed, but completely missing - as in nowhere to be found.  She immediately panics and go straight for the Herman Miller chair in which her wedding dress laid. After seeing the total annihilation  of the dress of her dreams she dropped to her knees, only to find the empty box that contained her wedding ring! she had taken it off while at the spa and now it was missing. She looked over to Barkley. Barkley could not even make eye contact. He knew he was a bad boy. Janet burst into tears. She wiped her face and looked down at a box she had never seen before. it was bright pink and wrapped in a erotic lace ribbon. Inside Janet sees countless notes labeled Chris and Chrissy-Poo covered in lipstick stains. The box smelled of cheap cologne and vodka. "Is Chris cheating on me!!!? she exclaimed.."</p>
+                <ul class="meta">
+                    <li>By: <a class="user" href="user-profile.php">MovieMaker</a></li>
+                    <li><a class="to-comments" href="scene-comments.php">26 Comments</a></li>
+                    <li><a class="read-more" id="show-hide" href="#">Read entire scene</a></li>
+                </ul>
+                <div class="img-r">
+                	<img src="images/recent_activity2_07.png" width="208" height="118" alt="scene">
+                </div>
+              </li>
+              <li class="single standard-listing">
+				<div class="avatar-l">
+                	<a href="#"><img src="images/user.png" width="74" height="74" alt="user"></a>
+                </div>
+                <h3>Upon arrival home from the spa, Janet walks into her 3 bedroom apartment and discovers Barkley has completed destroyed her wedding dress. Not only that but he has ate the wedding ring, and wrecked Janet's apartment. Janet sees all of this, but is in for the biggest surprise of all, a hidden box!</h3>
+                <p>When Janet comes home to Barkley and her apartment, she notices something is off. First of all her couch cushions are missing. Not destroyed, but completely missing - as in nowhere to be found.  She immediately panics and go straight for the Herman Miller chair in which her wedding dress laid. After seeing the total annihilation  of the dress of her dreams she dropped to her knees, only to find the empty box that contained her wedding ring! she had taken it off while at the spa and now it was missing. She looked over to Barkley. Barkley could not even make eye contact. He knew he was a bad boy. Janet burst into tears. She wiped her face and looked down at a box she had never seen before. it was bright pink and wrapped in a erotic lace ribbon. Inside Janet sees countless notes labeled Chris and Chrissy-Poo covered in lipstick stains. The box smelled of cheap cologne and vodka. "Is Chris cheating on me!!!? she exclaimed.."</p>
+                <ul class="meta">
+                    <li>By: <a class="user" href="user-profile.php">MovieMaker</a></li>
+                    <li><a class="to-comments" href="scene-comments.php">26 Comments</a></li>
+                    <li><a class="read-more" id="show-hide" href="#">Read entire scene</a></li>
+                </ul>
+                <div class="img-r">
+                	<img src="images/recent_activity2_07.png" width="208" height="118" alt="scene">
+                </div>
+              </li>
+              <li class="single standard-listing">
+				<div class="avatar-l">
+                	<a href="#"><img src="images/user.png" width="74" height="74" alt="user"></a>
+                </div>
+                <h3>Upon arrival home from the spa, Janet walks into her 3 bedroom apartment and discovers Barkley has completed destroyed her wedding dress. Not only that but he has ate the wedding ring, and wrecked Janet's apartment. Janet sees all of this, but is in for the biggest surprise of all, a hidden box!</h3>
+                <p>When Janet comes home to Barkley and her apartment, she notices something is off. First of all her couch cushions are missing. Not destroyed, but completely missing - as in nowhere to be found.  She immediately panics and go straight for the Herman Miller chair in which her wedding dress laid. After seeing the total annihilation  of the dress of her dreams she dropped to her knees, only to find the empty box that contained her wedding ring! she had taken it off while at the spa and now it was missing. She looked over to Barkley. Barkley could not even make eye contact. He knew he was a bad boy. Janet burst into tears. She wiped her face and looked down at a box she had never seen before. it was bright pink and wrapped in a erotic lace ribbon. Inside Janet sees countless notes labeled Chris and Chrissy-Poo covered in lipstick stains. The box smelled of cheap cologne and vodka. "Is Chris cheating on me!!!? she exclaimed.."</p>
+                <ul class="meta">
+                    <li>By: <a class="user" href="user-profile.php">MovieMaker</a></li>
+                    <li><a class="to-comments" href="scene-comments.php">26 Comments</a></li>
+                    <li><a class="read-more" id="show-hide" href="#">Read entire scene</a></li>
+                </ul>
+                <div class="img-r">
+                	<img src="images/recent_activity2_07.png" width="208" height="118" alt="scene">
+                </div>
+              </li>
+              <li class="single standard-listing">
+				<div class="avatar-l">
+                	<a href="#"><img src="images/user.png" width="74" height="74" alt="user"></a>
+                </div>
+                <h3>Upon arrival home from the spa, Janet walks into her 3 bedroom apartment and discovers Barkley has completed destroyed her wedding dress. Not only that but he has ate the wedding ring, and wrecked Janet's apartment. Janet sees all of this, but is in for the biggest surprise of all, a hidden box!</h3>
+                <p>When Janet comes home to Barkley and her apartment, she notices something is off. First of all her couch cushions are missing. Not destroyed, but completely missing - as in nowhere to be found.  She immediately panics and go straight for the Herman Miller chair in which her wedding dress laid. After seeing the total annihilation  of the dress of her dreams she dropped to her knees, only to find the empty box that contained her wedding ring! she had taken it off while at the spa and now it was missing. She looked over to Barkley. Barkley could not even make eye contact. He knew he was a bad boy. Janet burst into tears. She wiped her face and looked down at a box she had never seen before. it was bright pink and wrapped in a erotic lace ribbon. Inside Janet sees countless notes labeled Chris and Chrissy-Poo covered in lipstick stains. The box smelled of cheap cologne and vodka. "Is Chris cheating on me!!!? she exclaimed.."</p>
+                <ul class="meta">
+                    <li>By: <a class="user" href="user-profile.php">MovieMaker</a></li>
+                    <li><a class="to-comments" href="scene-comments.php">26 Comments</a></li>
+                    <li><a class="read-more" id="show-hide" href="#">Read entire scene</a></li>
+                </ul>
+                <div class="img-r">
+                	<img src="images/recent_activity2_07.png" width="208" height="118" alt="scene">
+                </div>
+              </li>
+              
+            </ul>
+            <div class="older-activity">
+                   		<a href="#" class="rounded-button">SEE OLDER</a>
+                        <a href="#" class="rounded-button">SEE NEWER</a>
+                   </div>
+            </div>
+          </section>
+        </div>
+    </div> <!--! end of #container -->
+    <div class="popup-container">
+                 <div class="login-popup post-popup">
+                 	<a href="#" class="close-popup"></a>
+                    <form action="post.php" method="get">
+                    	<h2>POST A NEW THREAD</h2>
+						<fieldset>
+                        	<label>CHOOSE A REPUBLIC ASSOCIATED WITH NEW THREAD</label>
+                        	<select class="sel" name="">
+                            	<option class="blt">Danny DiVito Project</option>
+                                <option>Danny DiVito Project 1</option>
+                                <option>Danny DiVito Project 2</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                        	<label>CHOOSE A REPUBLIC ASSOCIATED WITH NEW THREAD</label>
+                        	<select class="sel" name="">
+                            	<option>Danny DiVito Project</option>
+                                <option>Danny DiVito Project 1</option>
+                                <option>Danny DiVito Project 2</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                        	<label>SUBJECT</label>
+                        	<input name="" type="text" class="text-fld">
+                        </fieldset>
+                        <fieldset>
+                        	<textarea name="" cols="" rows=""></textarea>
+                        </fieldset>
+                        <input name="" type="image" src="images/post-button.png" class="join-button">
+                    </form>
+                 </div>
+             </div>
+    <!-- Inlcude Footer here... -->        
+    <?php include( 'footer.php' ); ?>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    
 	$('.trophy-entry a').hover(function() {
 	 	$(this).toggleClass('visible');
 	});
