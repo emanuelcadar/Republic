ef7cdeb36ede173ea493c2c162ed7fda7552cb06
Emanuel Cadar <ecadar@gmail.com>
2012-07-13 13:50:41 +0300

    Guilds Top Page

---
+++ guilds-top.php
@@ -0,0 +1,205 @@
+<!doctype html>  
+
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> 
+
+<?php  $currentPage = "film"; ?>
+<?php  $category = "film"; ?>
+<?php  $thisPage = "film"; ?>
+
+<html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="robots" content="noindex">
+    <title>Republic of Film</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-ipad.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-iphone4.png">
+
+    <link rel="stylesheet" href="css/style.css?v=2">
+    <link rel="stylesheet" href="css/superfish.css">
+    <!-- <link rel="stylesheet" href="css/sparkbox-select.css"> -->
+
+    <script src="js/libs/modernizr-1.7.min.js"></script>
+    
+    <!-- Grab Google CDN's jQuery. fall back to local if necessary -->    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
+    <script src="js/jquery.ui.selectmenu.js"></script>
+    <script src="js/jquery.vote.js"></script>
+    <script src="js/jquery.listmanager.js"></script>
+    <script src="js/jquery.hoverIntent.minified.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/script.js"></script>
+
+
+</head>
+
+<body>
+    <div class="container">
+        
+        <!-- Inlcude Header here... -->
+            
+        <?php include( 'header-simple.php' ); ?>
+            <div class="user-hero guilds-header">
+            	<div class="user-left">
+                	<span class="back-guilds"><a href="#" class="back-home">REPUBLIC HOME</a></span>
+                    <div class="clr"></div>
+                    <div class="user-info centered-ui group">
+                    	<h2>GUILDS OF THE REPUBLIC</h2>
+                    </div>
+                    <div class="clr"></div>
+                </div>
+                <div class="clr"></div>
+                <ul class="guilds-nav">
+                	<li><a href="#" class="active">TOP GUILDS</a></li>
+                    <li><a href="#">NEWLY CREATED GUILDS</a></li>
+                    <li><a href="#">MOST ELIGIBLE DRAFTEES</a></li>
+                </ul>
+            </div>  
+        </header>
+        
+        <div class="main">
+            <section>
+            	<div class="guilds-top">
+                	<h2>TOP GUILDS</h2>
+                    <div class="navigation-pitches group">
+                        <ul>
+                            <li><a href="#" class="active">LAST 30 DAYS</a></li>
+                            <li><a href="#">LAST 90 DAYS</a></li>
+                            <li><a href="#">LAST YEAR</a></li>
+                            <li><a href="#">ALL TIME</a></li>
+                            <li class="inf">POINTS</li>
+                            <li class="inf sec">WINNING PITCHES</li>
+                        </ul>
+                    </div>
+                    <div class="guilds-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry odd-g-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry odd-g-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry odd-g-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry odd-g-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                    <div class="guilds-entry odd-g-entry group">
+                    	<div class="avt">
+                        	<a href="#"><img src="images/guild-image.png" width="72" height="72" alt="img"></a>
+                        </div>
+                        <span class="g-name">BROKEN ARROW</span>
+                        <span class="g-points">4,537</span>
+                        <span class="g-points sec">35</span>
+						<div class="g-page">
+                        	<a href="#" class="rounded-button">GUILD PAGE</a>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="older-activity">
+                   		<a href="#" class="rounded-button">SEE TOP 50</a>
+                </div>
+          	</section>
+        </div>
+    </div> <!--! end of #container -->
+    <!-- Inlcude Footer here... -->        
+    <?php include( 'footer.php' ); ?>
